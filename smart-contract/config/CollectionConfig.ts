c5fb711f99ab2d32569a268cb8642710c2a648c4
0x017eth <0x017.eth@gmail.com>
2022-04-27 20:44:15 +0800

    My changes

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import * as Networks from '../lib/Networks';
+import * as Marketplaces from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: Networks.ethereumTestnet,
+  mainnet: Networks.ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'TheConstellationClub',
+  tokenName: 'Constellation Card',
+  tokenSymbol: 'TCC',
+  hiddenMetadataUri: 'ipfs://QmQBsKSGXVNnXxbRafGwxwfnWVDwLpZhhdZj9PCkur7SYV/hidden.json',
+  maxSupply: 1200,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 3,
+  },
+  publicSale: {
+    price: 0.15,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: null,
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: Marketplaces.openSea,
+  whitelistAddresses,
+};
+
+export default CollectionConfig;
